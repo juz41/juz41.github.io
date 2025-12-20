cecaa2be146e3be194af62cde04ee4d7acc4dfc6
juz41 <julek.zalewski@gmail.com>
2025-12-20 21:22:45 +0100

    inter, color modes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3482551908"
+    serviceWorkerVersion: "453340363"
   }
 });
